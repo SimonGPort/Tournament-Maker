ec5c1a9ee32c3cf4aa4e51ff829a24f92b6a83f3
SimonGPort <simon.giroux.portelance@gmail.com>
2020-05-12 16:37:45 -0400

    TM simple-peer connection 1 sec

--- src/Rooms.jsx
+++ src/Rooms.jsx
@@ -5,6 +5,8 @@ import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:4002");
 
+// let myStream = undefined;
+
 class Rooms extends Component {
   constructor() {
     super();
@@ -17,56 +19,12 @@ class Rooms extends Component {
     console.log("room mounted");
     socket.emit("roomMounted");
     socket.on("yourID", (id) => {
-      console.log("hello world");
       console.log("yourID", id);
       socket.emit("yourID+name", { id: id, name: this.props.user });
       this.props.dispatch({
         type: "myID",
         myID: id,
       });
-    });
-
-    socket.on("callReceived", (data) => {
-      console.log("callReceived", data);
-      console.log("callReceived from:", data.from);
-      const peer2 = new Peer({
-        initiator: false,
-        trickle: false,
-        stream: this.state.myStream,
-      });
-
-      peer2.signal(data.signal);
-
-      peer2.on("stream", (stream) => {
-        console.log(this.state.myStream);
-        console.log("inside of stream:", stream);
-        let audio = document.querySelector("audio");
-        audio.srcObject = stream;
-        // audio.srcObject = this.state.myStream;
-      });
-      let to = data.from;
-      peer2.on("signal", (data) => {
-        socket.emit("acceptCall", { to: to, signal: data });
-      });
-    });
-
-    socket.on("callAccepted", (data) => {
-      console.log("callAccepted yay");
-      const peer = new Peer({
-        initiator: true,
-        trickle: false,
-        stream: this.state.myStream,
-      });
-      // peer.signal(data);
-
-      peer.on("stream", (stream) => {
-        console.log(this.state.myStream);
-        console.log("inside of stream3:", stream);
-        let audio = document.querySelector("audio");
-        audio.srcObject = stream;
-        // audio.srcObject = this.state.myStream;
-      });
-      console.log("finish", data);
     });
 
     socket.on("personInTheRoom", (users) => {
@@ -78,41 +36,89 @@ class Rooms extends Component {
 
     navigator.mediaDevices
       .getUserMedia({
-        video: false,
-        audio: true,
+        video: true,
+        audio: false,
       })
       .then((stream) => {
         this.setState({ myStream: stream });
-
+        // myStream = stream;
+        console.log("render");
         // let video = document.querySelector("video");
         // video.srcObject = stream;
+        this.callPeer();
+      });
 
-        this.props.personInTheRoom.forEach((person) => {
-          if (person.name === this.props.user) {
-            return;
-          }
+    socket.on("callReceived", (data) => {
+      console.log("callReceived", data);
+      console.log("callReceived from:", data.from);
+      const peer2 = new Peer({
+        initiator: false,
+        trickle: false,
+        stream: this.state.myStream,
+        // stream: myStream,
+      });
 
-          const peer = new Peer({
-            initiator: true,
-            trickle: false,
-            stream: this.state.myStream,
-          });
-          peer.on("signal", (data) => {
-            socket.emit("callUser", {
-              userToCall: person.id,
-              signalData: data,
-              from: this.props.myID,
-            });
-          });
+      peer2.signal(data.signal);
+
+      peer2.on("stream", (stream) => {
+        console.log(this.state.myStream);
+        // console.log(myStream);
+        console.log("inside of stream:", stream);
+        let video = document.querySelector("video");
+        video.srcObject = stream;
+        audio.srcObject = stream;
+      });
+      let to = data.from;
+      peer2.on("signal", (data) => {
+        socket.emit("acceptCall", { to: to, signal: data });
+      });
+    });
+  }
+
+  callPeer = () => {
+    console.log("hello world2");
+    this.props.personInTheRoom.forEach((person) => {
+      if (person.name === this.props.user) {
+        return;
+      }
+      console.log("after person");
+      const peer = new Peer({
+        initiator: true,
+        trickle: false,
+        stream: this.state.myStream,
+        // stream:myStream
+      });
+      console.log("before signal");
+      peer.on("signal", (data) => {
+        socket.emit("callUser", {
+          userToCall: person.id,
+          signalData: data,
+          from: this.props.myID,
         });
       });
-  }
+
+      socket.on("callAccepted", (data) => {
+        console.log("callAccepted yay");
+        peer.signal(data);
+
+        peer.on("stream", (stream) => {
+          console.log(this.state.myStream);
+          // console.log(myStream);
+          console.log("inside of stream3 caller:", stream);
+          let video = document.querySelector("video");
+          video.srcObject = stream;
+          audio.srcObject = stream;
+        });
+        console.log("finish for the caller", data);
+      });
+    });
+  };
 
   render = () => {
     return (
       <div>
-        Room 0
-        <audio autoPlay />
+        Room 0{/* <audio autoPlay /> */}
+        <video autoPlay />
       </div>
     );
   };
